4b6575556df4712433b8d92bf4e4798a30390537
maskudo <manujkarki101@gmail.com>
2021-05-22 20:39:52 +0545

    created a 16*16 grid

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-const container = document.querySelector(".container")
+const gridContainer = document.querySelector(".container")
 let gridSize = 16
-const box = []
-for(let i=0;i<gridSize;i++){
+let box = []
+
+//creating grid boxes
+for(let i=0;i<gridSize*gridSize;i++){
     box[i] = document.createElement('div')
-    
+    box[i].setAttribute('class','gridItem')
+    gridContainer.append(box[i])
 }
+
+gridContainer.setAttribute('style',`grid-template-columns:repeat(${gridSize},auto)`)
+
+
+
