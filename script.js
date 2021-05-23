d86be45190a13ca943ec4bdd9ae9e0aa9a6bebe3
maskudo <manujkarki101@gmail.com>
2021-05-23 20:32:21 +0545

    can now edit grid size

--- script.js
+++ script.js
@@ -1,21 +1,43 @@
 const gridContainer = document.querySelector(".container")
 let gridSize = 16
 let box = []
+var value = 16
 
-//creating grid boxes
-for(let i=0;i<gridSize*gridSize;i++){
-    box[i] = document.createElement('div')
-    box[i].setAttribute('class','gridItem')
-    gridContainer.append(box[i])
+
+function createGrid(size){
+    gridSize = size
+    for(let i=0;i<gridSize*gridSize;i++){
+        box[i] = document.createElement('div')
+        box[i].setAttribute('class','gridItem')
+        gridContainer.append(box[i])
+    }
+    gridContainer.setAttribute('style',`grid-template-columns:repeat(${size},auto)`)
+    activateEffect()
+
+}
+function removeGrid(size){
+    for(let i=0;i<size*size;i++){
+        gridContainer.removeChild(box[i])
+    }
+}
+function activateEffect(){
+    let gridBoxList = document.querySelectorAll(".gridItem")
+    gridBoxList.forEach((item) =>{
+        item.addEventListener('mouseover',() => {
+            item.classList.add("darkened")
+        })
+    })
 }
 
-gridContainer.setAttribute('style',`grid-template-columns:repeat(${gridSize},auto)`)
-
-let gridBoxList = document.querySelectorAll(".gridItem")
-gridBoxList.forEach((item) =>{
-    item.addEventListener('mouseover',() => {
-        item.classList.add("darkened")
-    })
+const button = document.querySelector("#button")
+button.addEventListener('click',()=>{
+    value = prompt("Enter grid size(1-50): ")
+    if(value==null||value<=0||value>50){
+        value=gridSize
+    }
+    removeGrid(gridSize)
+    createGrid(value)
 })
 
+createGrid(gridSize)
 
