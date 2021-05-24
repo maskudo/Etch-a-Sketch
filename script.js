eefa78c2b0499462861d8c0751df332af3e5b4f9
maskudo <manujkarki101@gmail.com>
2021-05-24 21:54:48 +0545

    Grid now changes to random colors

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 const gridContainer = document.querySelector(".container")
 let gridSize = 16
 let box = []
-var value = 16
+let value = 16
 
+function genRanHex(){
+    let letters = "0123456789ABCDEF"
+    let color='#'
+    for(let i=0;i<6;i++){
+        color += letters[(Math.floor(Math.random() * 16))];
+    }
+    return color
+}
+
+function activateEffect(){
+    let gridBoxList = document.querySelectorAll(".gridItem")
+    gridBoxList.forEach((item) =>{
+        item.addEventListener('mouseover',() => {
+            //item.classList.add("darkened")
+            let color = genRanHex()
+            item.setAttribute("style",`background-color:${color}`)
+        })
+    })
+}
 
 function createGrid(size){
     gridSize = size
@@ -13,30 +32,29 @@ function createGrid(size){
     }
     gridContainer.setAttribute('style',`grid-template-columns:repeat(${size},auto)`)
     activateEffect()
-
 }
+
 function removeGrid(size){
     for(let i=0;i<size*size;i++){
         gridContainer.removeChild(box[i])
     }
 }
-function activateEffect(){
-    let gridBoxList = document.querySelectorAll(".gridItem")
-    gridBoxList.forEach((item) =>{
-        item.addEventListener('mouseover',() => {
-            item.classList.add("darkened")
-        })
-    })
-}
 
-const button = document.querySelector("#button")
-button.addEventListener('click',()=>{
+
+const changeSize = document.querySelector("#changeSize")
+changeSize.addEventListener('click',()=>{
     value = prompt("Enter grid size(1-50): ")
     if(value==null||value<=0||value>50){
         value=gridSize
     }
     removeGrid(gridSize)
     createGrid(value)
+})
+
+const clear = document.querySelector("#clear")
+clear.addEventListener('click',() => {
+    removeGrid(gridSize)
+    createGrid(gridSize)
 })
 
 createGrid(gridSize)
